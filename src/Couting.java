4d29a7963b1f35e590e424aa88f2761bcdc8d829
SteflerJiang <steflerjiang@163.com>
2016-06-20 17:36:54 +0800

    2.3

--- src/Couting.java
+++ src/Couting.java
@@ -1,17 +1,12 @@
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Couting {
 	public static void main(String[] args) throws InterruptedException {
-		class Counter {
-			private int count = 0;
-			public synchronized void increment() {++count;}
-			public int getCount() {return count;}
-		}
-		
-		final Counter counter = new Counter();
+		final AtomicInteger counter = new AtomicInteger();
 		class CountingThread extends Thread {
 			public void run() {
 				for (int i = 0; i < 10000; ++i) {
-					counter.increment();
+					counter.incrementAndGet();
 				}
 			}
 		}
@@ -20,6 +15,6 @@ public class Couting {
 		CountingThread t2 = new CountingThread();
 		t1.start(); t2.start();
 		t1.join(); t2.join();
-		System.out.println(counter.getCount());
+		System.out.println(counter.get());
 	}
 }
--- src/DiningPhilosophers.java
+++ src/DiningPhilosophers.java
@@ -1,32 +1,81 @@
 import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Philosopher extends Thread {
-	private Chopstick left, right;
+	private boolean eating;
+	private Philosopher left;
+	private Philosopher right;
+	private ReentrantLock table;
+	private Condition condition;
 	private Random random;
-	public int count;
+	private static int cnt = 1;
+	private int id;
 	
-	public Philosopher(Chopstick left, Chopstick right, int count) {
+	public Philosopher(ReentrantLock table) {
+		eating = false;
+		this.table = table;
+		condition = table.newCondition();
+		random = new Random();
+		this.id = cnt;
+		++cnt;
+	}
+	
+	public Philosopher(ReentrantLock table, Philosopher left, Philosopher right) {
+		eating = false;
+		this.table = table;
+		condition = table.newCondition();
+		random = new Random();
+		this.id = cnt;
+		++cnt;
 		this.left = left;
 		this.right = right;
-		this.count = count;
-		random = new Random();
+	}
+	
+	public void setLeft(Philosopher left) {
+		this.left = left;
+	}
+	
+	public void setRight(Philosopher right) {
+		this.right = right;
 	}
 	
 	public void run() {
 		try {
 			while (true) {
-				Thread.sleep(random.nextInt(1000));
-				System.out.println("thinking");
-				synchronized (left) {
-					synchronized (right) {
-						Thread.sleep(random.nextInt(1000));
-						System.out.println("eating");
-					}
-				}
+				think();
+				eat();
 			}
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			// TODO: handle exception
 		}
+	}
+	
+	private void think() throws InterruptedException {
+		table.lock();
+		try {
+			eating = false;
+			System.out.println("ph " + id + " is thinking");
+			left.condition.signal();
+			right.condition.signal();
+		} finally {
+			table.unlock();
+		}
+		Thread.sleep(random.nextInt(1000));
+	}
+	
+	private void eat() throws InterruptedException {
+		table.lock();
+		try {
+			while (left.eating || right.eating) {
+				condition.await();
+			}
+			eating =  true;
+			System.out.println("ph " + id + " is eatnig");
+		} finally {
+			table.unlock();
+		}
+		Thread.sleep(random.nextInt(1000));
 	}
 }
 
@@ -36,12 +85,22 @@ class Chopstick {}
 public class DiningPhilosophers {
 
 	public static void main(String[] args) {
-		Chopstick left = new Chopstick();
-		Chopstick right = new Chopstick();
-		for (int i = 0; i < 5; i++) {
-			Philosopher p = new Philosopher(left, right, i);
-			p.start();
-		}
+		ReentrantLock  table = new ReentrantLock();
+		Philosopher p1 = new Philosopher(table);
+		Philosopher p2 = new Philosopher(table);
+		Philosopher p3 = new Philosopher(table);
+		Philosopher p4 = new Philosopher(table);
+		Philosopher p5 = new Philosopher(table);
+		p1.setLeft(p5);p1.setRight(p2);
+		p2.setLeft(p1);p2.setRight(p3);
+		p3.setLeft(p2);p3.setRight(p4);
+		p4.setLeft(p3);p4.setRight(p5);
+		p5.setLeft(p4);p5.setRight(p1);
+		p1.start();
+		p2.start();
+		p3.start();
+		p4.start();
+		p5.start();
 	}
 
 }
